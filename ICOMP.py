6dcf8f745204b76badb1e6aa82fa04be5ced2850
Pongpisit Thanasutives <pongpisit.tha@gmail.com>
2024-02-21 22:09:38 +0900

    Fix bugs in ICOMP

--- ICOMP.py
+++ ICOMP.py
@@ -1,6 +1,7 @@
 import numpy as np
 import statsmodels.api as sm
 from scipy.stats import gmean
+from scipy.linalg import solve
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.metrics.pairwise import pairwise_kernels
 
@@ -26,7 +27,7 @@ def OLS_icomp(X, y, X_cov=None):
     return 2*(icomp-pll)
 
 class KRR(object):
-    def __init__(self, alpha=1.0):
+    def __init__(self, alpha=0.1):
         self.alpha = alpha
         self.nobs = None
         self.A = None
@@ -38,7 +39,7 @@ class KRR(object):
     def fit(self, K, y):
         self.nobs = len(y)
         self.A = K + self.alpha * np.eye(len(K))
-        self.dual_coef_ = scipy.linalg.solve(self.A, y, assume_a='sym')
+        self.dual_coef_ = solve(self.A, y, assume_a='sym')
         self.train_residual = y - self.predict(K)
         self.kic(K, self.dual_coef_, self.train_residual, self.A)
         return self
@@ -50,10 +51,10 @@ class KRR(object):
         nobs2 = self.nobs/2
         var = np.dot(y_res, y_res)
         tKt = theta.dot(K).dot(theta)
-        ss = (var + krr.alpha*tKt)/self.nobs
+        ss = (var + self.alpha*tKt)/self.nobs
         A = np.linalg.inv(A)
         sigma_theta = K.dot(A.dot(A))
-        self.pll = -nobs2*np.log(2*np.pi*ss)-(var+krr.alpha*tKt)/(2*ss)
+        self.pll = -nobs2*np.log(2*np.pi*ss)-(var+self.alpha*tKt)/(2*ss)
         self.kic_1 = -2*self.pll+ss*np.trace(sigma_theta)
         self.kic_2 = -2*self.pll+ss*np.trace(sigma_theta.dot(sigma_theta))
 
